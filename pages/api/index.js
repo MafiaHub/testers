e850162f5536498448fe887bc97692db232d0076
Vladyslav Hrytsenko <vladgritsenko@gmail.com>
2019-09-25 20:21:39 +0300

    dbg

--- pages/api/index.js
+++ pages/api/index.js
@@ -4,6 +4,8 @@ import {loadCache, bustCache} from 'utils/cache'
 export default async (req, res) => {
     const {projects} = config
 
+    console.log(process.env)
+
     if (req.query.cache == 'bust') {
         return bustCache(req, res)
     }
